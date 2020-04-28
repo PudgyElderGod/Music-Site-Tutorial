990d188025e6ad40353f8ef906c945121362fb3c
PudgyElderGod <PudgyElderGod@gmail.com>
2020-04-27 21:35:26 -0700

    Boatload of models added

--- music/models.py
+++ music/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+#Models here
+class Artist(models.Model):
+    name = models.CharField(max_length=50)
+    formation_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=200)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    genre = models.CharField(max_length=50)
+    publish_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Song(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.title
